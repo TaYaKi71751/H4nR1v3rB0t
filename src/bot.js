d5ab8dbe46676176f164a4a39fa2cb4fab6bdc7a
TaYaKi71751 <shantian.tw@gmail.com>
2020-05-10 11:47:31 +0900

    날짜 및 시간이 잘리는 버그 개선

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(bot_token, { polling: true });
 var exec = require('child_process').exec, child;
-var hanriver;
+var hanriver, htime;
+const datee = ["년 ","월 ","일\n"];
+const timee = ["시 ","분 ","초 "];
 const URI = "http://hangang.dkserver.wo.tc/";
 const helpMessage = "Keywords : {" + "\n" + "\t한강,\n" + "\t\*대교\n" + "}\n" + "Commands : {" + "\n" + "\t\/help " + ": Help Command" + "\n" + "\t/hanriver_temperature " + ": GET Han River's Temperature" + "\n" + "}";
 bot.on('message', (msg) => {
@@ -19,26 +21,24 @@ bot.on('message', (msg) => {
             + ")",
             function (error, stdout, stderr) {
                 hanriver = JSON.parse(stdout);
+                htime = hanriver.time.split(" ");
+                date = htime[0].split("-");
+                time = htime[1].split(":");
                 console.log("Triggered\n");
-                bot.sendMessage(chatId, "한강 물 온도는 섭씨 "
-                    + hanriver.temp + " 도 입니다.\n"
-                    + hanriver.time.substring(0, 4)
-                    + "년 "
-                    + hanriver.time.substring(hanriver.time.substring(5, 6) === "0" ? 6 : 5, 7)
-                    + "월 "
-                    + hanriver.time.substring(hanriver.time.substring(9, 10) === "0" ? 9 : 8, 10)
-                    + "일 "
-                    + hanriver.time.substring(hanriver.time.substring(11, 12 === "0" ? 11 : 10), 13)
-                    + "시 "
-                    + hanriver.time.substring(hanriver.time.substring(14, 15) === "0" ? 15 : 14, 16)
-                    + "분 "
-                    + hanriver.time.substring(hanriver.time.substring(17, 18) === "0" ? 18 : 17, 19)
-                    + "초 "
-                    + "측정"
+                message = "한강 물 온도는 섭씨 " + hanriver.temp + " 도 입니다.\n";
+                for(var i = 0;i<date.length;i++)
+                    message += date[i].indexOf("0") === 0 
+                     ? date[i].substring(date[i].indexOf("0") + 1,date[i].length) + datee[i]
+                      : date[i] + datee[i];
+                for(var i = 0;i<time.length;i++)
+                    message += time[i].indexOf("0") === 0
+                     ? time[i].substring(time[i].indexOf("0") + 1,time[i].length) + timee[i]
+                      : time[i] + timee[i];
+                    message += "측정"
                     + "\n"
                     + "출처 : "
-                    + URI
-                );
+                    + URI;
+                bot.sendMessage(chatId,message);
             });
         child();
 
