57ac0fef4e4a2d88b2725148753e6edaebb6f535
TaYaKi71751 <shantian.tw@gmail.com>
2020-07-19 16:14:47 +0900

    HanRiverBot

--- readme.md
+++ readme.md
@@ -3,14 +3,14 @@
 >## Config
 ><pre>
 ><code>
->const bot_token = '$YOUR_TELEGRAM_BOT_TOKEN';
+>const token = 'YOUR_DISCORD_BOT_TOKEN';
 ></code>
 ></pre>
 
 >## Run
 ><pre>
 ><code>
->npm install node-telegram-bot-api
+>npm install 
 >npm start
 ></code>
 ></pre>
--- src/bot.js
+++ src/bot.js
@@ -1,46 +1,34 @@
-const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(bot_token, { polling: true });
-var exec = require('child_process').exec, child;
-var hanriver, htime;
-const datee = ["년 ","월 ","일\n"];
-const timee = ["시 ","분 ","초 "];
-const URI = "http://hangang.dkserver.wo.tc/";
-const helpMessage = "Keywords : {" + "\n" + "\t한강,\n" + "\t\*대교\n" + "}\n" + "Commands : {" + "\n" + "\t\/help " + ": Help Command" + "\n" + "\t/hanriver_temperature " + ": GET Han River's Temperature" + "\n" + "}";
-bot.on('message', (msg) => {
-    var chatId = msg.chat.id;
-    if (msg.text.indexOf("\/help") === 0)
-        bot.sendMessage(chatId, helpMessage);
-    if (msg.text && msg.text.includes("한강")
-        || msg.text.indexOf("대교") > 0
-        || msg.text.indexOf("\/hanriver_temperature") === 0) {
-        child = exec("echo "
-            + "$(curl "
-            + "-LsSfH "
-            + "'Cache-Control: no-cache' "
-            + URI
-            + ")",
-            function (error, stdout, stderr) {
-                hanriver = JSON.parse(stdout);
-                htime = hanriver.time.split(" ");
-                date = htime[0].split("-");
-                time = htime[1].split(":");
-                console.log("Triggered\n");
-                message = "한강 물 온도는 섭씨 " + hanriver.temp + " 도 입니다.\n";
-                for(var i = 0;i<date.length;i++)
-                    message += date[i].indexOf("0") === 0 
-                     ? date[i].substring(date[i].indexOf("0") + 1,date[i].length) + datee[i]
-                      : date[i] + datee[i];
-                for(var i = 0;i<time.length;i++)
-                    message += time[i].indexOf("0") === 0
-                     ? time[i].substring(time[i].indexOf("0") + 1,time[i].length) + timee[i]
-                      : time[i] + timee[i];
-                    message += "측정"
-                    + "\n"
-                    + "출처 : "
-                    + URI;
-                bot.sendMessage(chatId,message);
-            });
-        child();
-
-    }
+const token = 'YOUR_DISCORD_BOT_TOKEN';
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require('node-fetch');
+const URI = 'http://hangang.dkserver.wo.tc';
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
+
+client.on('message', msg => {
+  if (msg.content.includes("한강")) {
+    fetch(URI)
+        .then(res =>res.json())
+        .then(data => {
+            var time = data.time;
+            while(time = time.  replace("-"," ").replace(":"," "))
+                if(!time.includes(":")&&!time.includes("-")) {
+                    time=time.split(" ");
+                    break;
+                }
+            msg.reply("한강 수온은"
+          + "\n" + "\t" + "섭씨" + data.temp + "도 입니다."
+          + "\n" + "\t" + time[0] + " 년 "
+                        + (time[1].indexOf("0") === 0 ? time[1].charAt(1).toString() : time[1]) + " 월 "
+                        + (time[2].indexOf("0") === 0 ? time[2].chatAt(1).toString() : time[2]) + " 일 "
+          + "\n" + "\t" + (time[3].indexOf("0") === 0 ? time[3].charAt(1).toString() : time[3]) + " 시 "
+                        + (time[4].indexOf("0") === 0 ? time[4].charAt(1).toString() : time[4]) + " 분 "
+                        + (time[5].indexOf("0") === 0 ? time[5].charAt(1).toString() : time[5]) + " 초 " + "측정"
+                 + "\n" + "출처 : " + URI);
+        });
+  }
+});
+
+client.login(token);
