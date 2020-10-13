a5e50720565668421deb090948e2c851cac44c38
TaYaKi71751 <shantian.tw@gmail.com>
2020-10-12 21:17:45 -0400

    Bot

---
+++ src/bot.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const fetch = require('node-fetch');
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.bot_token, { polling: true });
+bot.on('inline_query', function (query) {
+    inlineQueryString = "";
+    fetch('http://127.0.0.1:8080/koreawqijson/temperature/river')
+        .then(response => response.json())
+        .then((data) => {
+            inlineQueryString = "";
+            inlineQueryString += "[";
+            for (var i = 0; i < data["Han"].length; i++) {
+                inlineQueryString += "{" +
+                    "\"id\": " + "\"" + i + "\"," +
+                    "\"type\": " + "\"article\"," +
+                    "\"title\": " + "\"" + data["Han"][i].name + "\"," +
+                    "\"description\": " + "\"" + (data["Han"][i].temp.trim() === "정보없음" ? data["Han"][i].temp.trim() : data["Han"][i].temp.trim() + "℃") + "\"," +
+                    "\"message_text\": " + "\"" + "[" + data["Han"][i].name + "]\\n" + (data["Han"][i].temp.trim() === "정보없음" ? data["Han"][i].temp.trim() : data["Han"][i].temp.trim() + "℃") + "\\n" + "(" + data.date.substring(0, data.date.length - 4) + "년 " + data.date.substring(data.date.length - 4, data.date.length - 2) + "월 " + data.date.substring(data.date.length - 2, data.date.length) + "일" + ")" + "\"";
+                inlineQueryString += (data["Han"].length - 1 === i ? "}]" : "},");
+            }
+            bot.answerInlineQuery(query.id, JSON.parse(inlineQueryString), { cache_time: 0 });
+        })
+});
